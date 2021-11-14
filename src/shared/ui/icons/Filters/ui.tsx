56d4e1d03eba127b2231ca39857c93a0dd7aa1b2
Constantine <77288439+cgradusov@users.noreply.github.com>
2021-11-14 15:46:06 +0300

    feat: Add Filters icon

---
+++ src/shared/ui/icons/Filters/ui.tsx
@@ -0,0 +1,9 @@
+/* eslint-disable import/prefer-default-export */
+import React from 'react';
+import { Svg } from '../Svg';
+
+export const FiltersIcon: React.FC<any> = ({ className }) => (
+  <Svg width="23" height="20" viewBox="0 0 23 20" fill="none" className={className}>
+    <path d="M3.5 19V12M3.5 8V1M11.5 19V10M11.5 6V1M19.5 19V14M19.5 10V1M0.5 12H6.5M8.5 6H14.5M16.5 14H22.5" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" />
+  </Svg>
+);
