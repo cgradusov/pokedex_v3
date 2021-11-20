3b435dc8b5b02644f38d3bbbb2aceea6420ed495
Constantine <77288439+cgradusov@users.noreply.github.com>
2021-11-20 13:51:34 +0300

    feat: add pokemon stats formater util

---
+++ src/utils/pokeStatsFormater.ts
@@ -0,0 +1,36 @@
+type PokeStat = 'hp' | 'attack' | 'defense' | 'special-attack' | 'special-defense' | 'speed';
+
+type StatNameMap = {
+  // eslint-disable-next-line no-unused-vars
+  [key in PokeStat]: string;
+};
+
+export type Stat = {
+  name: PokeStat;
+  bs: number;
+}
+
+const statNameMap: StatNameMap = {
+  hp: 'hp',
+  attack: 'attack',
+  defense: 'defence',
+  'special-attack': 'specialAttack',
+  'special-defense': 'specialDefence',
+  speed: 'speed',
+};
+
+/**
+ *
+ * @param stats
+ * @returns Stats object with remaped name and lowered by 0.6 value
+ */
+
+const statsFormater = (stats: Stat[]) => stats.reduce((acc: any, s: Stat) => {
+  const { bs, name: sn } = s;
+  const statName = statNameMap[sn];
+  acc[statName] = bs * 0.6;
+
+  return acc;
+}, {});
+
+export default statsFormater;
